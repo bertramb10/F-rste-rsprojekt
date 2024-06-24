5e9cfdd5b0dc66cc281ea799661a475fe59cac5b
bertramb10 <150904996+bertramb10@users.noreply.github.com>
2024-06-24 20:50:42 +0200

    Create MUN Sletbruger.py

---
+++ MUN Sletbruger.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def delete_user(rfid):
+    conn = sqlite3.connect('access_control.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM employees WHERE rfid=?", (rfid,))
+    conn.commit()
+    conn.close()
+    print(f"Bruger med RFID {rfid} slettet")
+
+if __name__ == '__main__':
+    
+    rfid_to_delete = '692663892787'
+    delete_user(rfid_to_delete)
