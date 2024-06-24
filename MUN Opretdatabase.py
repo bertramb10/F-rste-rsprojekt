0cac4918a76c9b4da328f53090f8775be93887a0
bertramb10 <150904996+bertramb10@users.noreply.github.com>
2024-06-24 20:52:27 +0200

    Create MUN Opretdatabase.py

---
+++ MUN Opretdatabase.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def init_db():
+    conn = sqlite3.connect('access_control.db')
+    c = conn.cursor()
+    c.execute('''
+    CREATE TABLE IF NOT EXISTS employees (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        rfid TEXT NOT NULL UNIQUE,
+        pin TEXT NOT NULL,
+        access_level INTEGER NOT NULL
+    )
+    ''')
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    init_db()
+    print("Database og tabel oprettet.")
